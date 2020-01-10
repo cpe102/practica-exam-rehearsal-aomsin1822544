b8e5be1e6deb24fa122a805fca9da2f1ffb7071d
aomsin1822544 <pspsas333_111@hotmail.com>
2020-01-10 17:14:24 +0700

    1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,27 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){ 
+string n ;
+  cout << "What is your name?";
+  cin >> n;
+double a;
+  cout << "What is your GPA?";
+  cin >> a;  
+}   
+ if(>=3.50){
+      cout << n <<" InW Jrim Jrim!!!";
+} else{
+      cout  << "Try harder," << x << "!!!";
+}
+}
+return 0;
+ 
+
+
+}
+
+
+
 
